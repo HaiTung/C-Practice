b9c719820420b246f6f673be459cea12f460ccf2
HaiTung <vphaitung@gmail.com>
2019-01-02 09:26:26 +0700

    +inforStudent

--- StudentManagement/Main.cpp
+++ StudentManagement/Main.cpp
@@ -1,19 +1,39 @@
-#include<iostream>
-#include<vector>
-#include<string>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
 
 using namespace std;
-
-void question();
-void getInforStudent();
-void showStudents(vector<Student> students);
+const string FILE_PATH = "Student_manager.txt";
 
 struct Student
 {
-	int _id;
-	string _name;
-	float _score;
+	int ID;
+	string Name;
+	float Score;
 };
+
+vector<Student> students;
+bool wantExit = false;
+
+void question();
+void showStudents(vector<Student> students);
+void getInforStudent();
+void continueInsertStudent();
+bool checkIdExit(int id, vector<Student> students);
+bool insertStudent(Student student, vector<Student> *students);
+bool saveStudentsToFile(vector<Student> students);
+bool loadStudentsFromFile(vector<Student> *students);
+
+int main() {
+	while (!wantExit)
+	{
+		question();
+	}
+	cout << "Thank for use ... " << endl;
+	system("pause");
+	return 0;
+}
 
 void question() {
 	system("cls");
@@ -55,4 +75,127 @@ void question() {
 	default:
 		break;
 	}
+}
+
+//
+void getInforStudent() {
+	system("cls");
+	cout << "----- INSERT STUDENT -----" << endl;
+	cout << "BIGGEST ID : " << students.size() << endl;
+	int id;
+	string name;
+	float score;
+
+	cout << "ID: "; cin >> id; cin.get();
+
+	if (checkIdExit(id, students)) {
+		cout << "Name: "; getline(cin, name);
+		cout << "score: "; cin >> score;
+
+		Student  student;
+		student.ID = id;
+		student.Name = name;
+		student.Score = score;
+
+		if (insertStudent(student, &students)) {
+			cout << "Insert Student success!" << endl;
+			continueInsertStudent();
+		}
+		else cout << "Insert Student Failed!" << endl;
+	}
+	else {
+		cout << "This ID is already Exist, Please try input." << endl;
+		continueInsertStudent();
+		return;
+	}
+}
+
+
+bool checkIdExit(int id, vector<Student> students) {
+	for (int i = 0; i < students.size(); i++) {
+		if (students.at(i).ID == id)
+			return false;
+	}
+	return true;
+}
+
+
+bool insertStudent(Student student, vector<Student> *students) {
+	int oldSize = students->size();
+	students->push_back(student);
+
+	if (oldSize < students->size())
+		return true;
+	return false;
+}
+
+void continueInsertStudent() {
+	cout << "You want continue insert student? (Y/N)";
+
+	char select;
+	cin >> select;
+	if (select == 'Y') getInforStudent();
+}
+
+void showStudents(vector<Student> students) {
+	system("cls");
+	cout << "ID\t\tFull Name\t\tScore" << endl;
+	for (int i = 0; i < students.size(); i++) {
+		cout << students.at(i).ID << "\t\t";
+		cout << students.at(i).Name << "\t\t";
+		cout << students.at(i).Score << endl;
+	}
+	cout << endl << endl;
+	system("pause");
+}
+
+bool saveStudentsToFile(vector<Student> students) {
+	ofstream insertToFile(FILE_PATH, std::ios::out);
+	for (int i = 0; i < students.size(); i++) {
+		insertToFile << students.at(i).ID << endl;
+		insertToFile << students.at(i).Name << endl;
+		insertToFile << students.at(i).Score << endl;
+		if (insertToFile.fail())
+			return false;
+	}
+
+	insertToFile.close();
+	return true;
+}
+
+bool loadStudentsFromFile(vector<Student> *students) {
+	int oldSize = students->size();
+	ifstream fileToList(FILE_PATH);
+	if (fileToList.fail()) return false;
+	int numLine = 0;
+	Student student;
+	string line;
+	int id;
+	float score;
+	while (getline(fileToList, line))
+	{
+		numLine++;
+		switch (numLine)
+		{
+		case 1:
+			id = stoi(line);
+			student.ID = id;
+			break;
+		case 2:
+			student.Name = line;
+			break;
+		case 3:
+			score = stof(line);
+			student.Score = score;
+			numLine = 0;
+			students->push_back(student);
+			break;
+		default:
+			break;
+		}
+	}
+
+	fileToList.close();
+	if (oldSize < students->size()) return true;
+	else return false;
 }
