5c17083ca9f868d60819339fb5434c6d32cb5965
HaiTung <vphaitung@gmail.com>
2019-01-08 05:49:18 +0700

    Updete virus.cpp

--- DemoOOP/Virus/Virus.cpp
+++ DemoOOP/Virus/Virus.cpp
@@ -14,33 +14,11 @@ Virus::Virus()
 }
 
 
-Virus::~Virus()
-{
-}
-
-Virus::Virus(Virus *virus)
-{
-	this->m_dna = virus->m_dna;
-	this->m_resistance = virus->m_resistance;
-}
-
-void Virus::loadADNInformation()
-{
-	string loca;
-	ifstream file;
-	file.open("ATGX.bin");
-
-	while (!file.eof())
-	{
-		getline(file, loca);
-		
-		this->m_dna = new char[loca.size() + 1];
-		this->m_dna = const_cast<char *> (loca.c_str());
-		file.close();
-	}
-}
-
-int Virus::reducResistance(int  medicine_resistance)
-{
-	return m_resistance = m_resistance - medicine_resistance;
+void Virus::LoadADNInformation() {
+	char * n = new char[225];
+	ifstream infile;
+	infile.open("ATGX.bin");
+	infile >> n;
+	cout << n;
+	infile.close();
 }
